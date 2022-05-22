f565e202d8837eca9d4364aa5d7fc1c64c7692cd
Nirav <nirav.sculptsoft@gmail.com>
2022-05-22 22:02:12 +0530

    > second commit

--- app/src/main/java/com/example/remotebased8/MainActivity.kt
+++ app/src/main/java/com/example/remotebased8/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.remotebased8
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+// second commit
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
